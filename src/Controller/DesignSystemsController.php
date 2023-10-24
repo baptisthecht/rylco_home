ad6673b85b45102a1537db2efb09d5dbb61aa7b1
Baptist HECHT <me@baptisthecht.fr>
2023-10-24 17:10:10 +0200

    FCT: Adding design systems pages

--- src/Controller/DesignSystemsController.php
+++ src/Controller/DesignSystemsController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Component;
+use App\Entity\DesignSystem;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -15,6 +18,20 @@ class DesignSystemsController extends AbstractController
         $this->denyAccessUnlessGranted('ROLE_CLIENT');
         return $this->render('design_systems/index.html.twig', [
             'controller_name' => 'DesignSystemsController',
+        ]);
+    }
+
+    #[Route('/designsystem/{id}', name: 'app_show_design_systems')]
+    public function showDS(int $id, ManagerRegistry $doctrine): Response
+    {
+        $this->denyAccessUnlessGranted('ROLE_CLIENT');
+
+        $repository = $doctrine->getRepository(DesignSystem::class);
+        $ds = $repository->find($id);
+
+        return $this->render('design_systems/show.html.twig', [
+            'controller_name' => 'DesignSystemsController',
+            'ds' => $ds
         ]);
     }
 }
