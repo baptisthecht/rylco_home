19cfc0a33e94f72812c90c889a2aa21b984db7a4
Baptist HECHT <me@baptisthecht.fr>
2023-11-28 17:32:33 +0100

    Added TOS

--- src/Controller/MentionsLegalesController.php
+++ src/Controller/MentionsLegalesController.php
@@ -6,13 +6,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class TosController extends AbstractController
+class MentionsLegalesController extends AbstractController
 {
-    #[Route('/tos', name: 'app_tos')]
+    #[Route('/mentions-legales', name: 'app_mentions_legales')]
     public function index(): Response
     {
-        return $this->render('tos/index.html.twig', [
-            'controller_name' => 'TosController',
+        return $this->render('mentions_legales/index.html.twig', [
+            'controller_name' => 'MentionsLegalesController',
         ]);
     }
 }
